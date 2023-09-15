b263334a8510c02ddb92e324fda204f7531bd762
Dilmer Valecillos <dilmervst@gmail.com>
2023-09-15 14:52:11 -0600

    Initial project files

---
+++ Inspiration4/Inspiration4App.swift
@@ -0,0 +1,17 @@
+//
+//  Inspiration4App.swift
+//  Inspiration4
+//
+//  Created by Dilmer Valecillos on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Inspiration4App: App {
+    var body: some Scene {
+        WindowGroup {
+            
+        }
+    }
+}
